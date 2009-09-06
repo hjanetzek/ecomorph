4264b7eb6ba10c4f81be36bad982cbe6461d0783
jeff <jeff@rappelkiste.(none)>
2009-09-06 21:46:22 +0200

    added second shadow match option

--- src/eco_config_decoration.c
+++ src/eco_config_decoration.c
@@ -20,9 +20,16 @@ eco_config_decoration(void *data)
   ECO_CREATE_SLIDER_INT(-1,    shadow_x_offset, "Offset X", -16, 16, "%1.0f", 0, 2);
   ECO_CREATE_SLIDER_INT(-1,    shadow_y_offset, "Offset Y", -16, 16, "%1.0f", 0, 3);
   ECO_CREATE_ENTRY(-1, shadow_match, "Match Windows", 0, 4);
-  
   ECO_PAGE_TABLE_END;
-  ECO_PAGE_END;
+  
+  ECO_PAGE_TABLE("Second Shadow (e.g. Menus))");
+  ECO_CREATE_SLIDER_DOUBLE(-1, menu_shadow_radius, "Radius", 0.0, 48.0, "%1.1f", 0, 0);
+  ECO_CREATE_SLIDER_DOUBLE(-1, menu_shadow_opacity, "Opacity", 0.0, 1.0, "%1.2f", 0, 1);
+  ECO_CREATE_SLIDER_INT(-1,    menu_shadow_x_offset, "Offset X", -16, 16, "%1.0f", 0, 2);
+  ECO_CREATE_SLIDER_INT(-1,    menu_shadow_y_offset, "Offset Y", -16, 16, "%1.0f", 0, 3);
+  ECO_CREATE_ENTRY(-1,         menu_shadow_match, "Match Windows", 0, 4);
+  ECO_PAGE_TABLE_END;
 
+  ECO_PAGE_END;
 }
 
